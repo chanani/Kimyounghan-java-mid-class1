ed2b07567cc7b108ed803a9d7ffebbe0f31a1d6f
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-06-21 13:24:05 +0900

    김영한님의 실전 자바 중급1편 강의에 대한 파일 : Object에 대해

---
+++ src/lang/object/Parent.java
@@ -0,0 +1,11 @@
+package lang.object;
+
+// 부모가 없으면 묵시적으로 OBject 클래스르 상속 받는다.
+// extends Object와 같다
+public class Parent {
+
+    public void parentMethod(){
+        System.out.println("parent.parentMethod");
+    }
+
+}
