92d7981a02fa5549a0db383d2d64f8c11f212b1b
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-07-03 22:35:29 +0900

    StringBuilder 복습 및 코드 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+//        String result = "";
+//        for (int i = 0; i < 100000; i++) {
+//            result += "hihihihihi";
+//        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 100000; i++) {
+            sb.append("hihihihihi");
+        }
+        String result = sb.toString();
+        long endTime = System.currentTimeMillis();
+
+        System.out.println(endTime - startTime + "ms");
 
     }
 }
