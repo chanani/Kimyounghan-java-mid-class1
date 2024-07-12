cfdb57da73db3fbdb2f01a0967cef682dd4a5f86
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-07-12 22:50:17 +0900

    Enum 리펙토링1

---
+++ src/enumeration/ref2/EnumRefMain2.java
@@ -0,0 +1,20 @@
+package enumeration.ref2;
+
+
+
+import static enumeration.ex3.Grade.*;
+
+public class EnumRefMain2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount(Grade.BASIC, price);
+        int gold = discountService.discount(Grade.GOLD, price);
+        int diamond = discountService.discount(Grade.DIAMOND, price);
+
+        System.out.println("BASIC 등금의 할인 급액 : " + basic);
+        System.out.println("GOLD 등금의 할인 급액 : " + gold);
+        System.out.println("DIAMOND 등금의 할인 급액 : " + diamond);
+    }
+}
