a799672a0a1d41248659a99a51589ad2901fd21f
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-07-17 23:18:35 +0900

    Period, Duration에 대해 이론 학습 및 실습

---
+++ src/time/DurationMain.java
@@ -0,0 +1,33 @@
+package time;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class DurationMain {
+
+    public static void main(String[] args) {
+        // 두 시간 사이의 간격을 시, 분, 초(나노초) 단위로 나타낸다.
+
+        // 생성
+        Duration duration = Duration.ofMinutes(30);
+        System.out.println("duration = " + duration);
+
+        LocalTime lt = LocalTime.of(1, 0);
+        System.out.println("lt = " + lt);
+
+        // 계산에 사용
+        LocalTime plusTime = lt.plus(duration);
+        System.out.println("더한 시간 : " + plusTime);
+
+        // 시간 차이
+        LocalTime start = LocalTime.of(9, 0);
+        LocalTime end = LocalTime.of(10, 0);
+        Duration between = Duration.between(start, end);
+        System.out.println("between = " + between);
+        System.out.println("차이 : " + between.getSeconds() + "초");
+        // toMillisPart : 시간을 제외한 분
+        System.out.println("근무 시간 : " + between.toHours() + "시간 " + between.toMillisPart() + "분");
+
+    }
+
+}
