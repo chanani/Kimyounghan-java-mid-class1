4e18ddf59b16cbcfb8a65d572efab9831bd5450e
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-07-21 21:37:26 +0900

    지역 클래스의 지역 변수 캡처 3

---
+++ src/nested/local/LocalOuterV4.java
@@ -0,0 +1,54 @@
+package nested.local;
+
+import java.lang.reflect.Field;
+
+public class LocalOuterV4 {
+
+    private int outInstanceVar = 3;
+
+    public Printer process(int paramVar){
+        // 지역 변수
+        // 사실상 final
+        int localVar = 1; // 지역 변수는 스택 프레임이 종료되는 순간 함께 제거된다.
+
+        // 지역 클래스
+        // 인터페이스도 구현 가능하다
+        class LocalPrinter implements Printer{
+            int value = 0;
+
+            @Override
+            public void print() {
+                System.out.println("value = " + value);
+
+                // 인스턴스는 지역 변수보다 더 오래 살아 남는다.
+                System.out.println("localVar = " + localVar);
+                System.out.println("paramVar = " + paramVar);
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+        }
+
+        LocalPrinter printer = new LocalPrinter();
+        // 만약 localVar의 값을 변경한다면 ? 다시 캡처해야 하나 ?
+        // localVar = 10;
+        // paramVar = 10;
+        return printer;
+    }
+ 
+    public static void main(String[] args) {
+        LocalOuterV4 localOuter = new LocalOuterV4();
+        Printer printer = localOuter.process(2);
+        // printer.print()를 나중에 실행한다. process()의 스택 프레임이 사라진 이후에 실행
+        printer.print();
+
+        // 추가
+        // 지역 변수 캡처 확인
+        System.out.println("필드 확인");
+        Field[] fields = printer.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println("field = " + field);
+        }
+        // 아래 출력 내용은 바깥 클래스를 참조하는 내용
+        // field = final nested.local.LocalOuterV3 nested.local.LocalOuterV3$1LocalPrinter.this$0
+
+    }
+}
