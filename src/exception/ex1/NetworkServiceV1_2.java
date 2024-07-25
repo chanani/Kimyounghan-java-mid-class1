c438ce6c020ef302311204cafd1575a9b4d89a98
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-07-25 23:28:11 +0900

    예외처리가 필용한 이유 이론 및 실습3

---
+++ src/exception/ex1/NetworkServiceV1_2.java
@@ -0,0 +1,34 @@
+package exception.ex1;
+
+
+
+public class NetworkServiceV1_2 {
+
+    public void sendMessage(String data){
+        // 복잡한 NetWorkClient 사용 로직을 처리합니다.
+        String address = "http://example.com";
+        NetworkClientV1 client = new NetworkClientV1(address);
+        client.initError(data); // 에러 전달
+
+        String connectResult = client.connect();
+        // 결과가 성공이 아니다 -> Error
+        // if(!connectResult.equals("success"))
+        if(isError(connectResult)){
+            System.out.println("[네트워크 오류 발생] 오류 코드 : " + connectResult);
+            return;
+        }
+
+        String sendResult = client.send(data);
+        if(isError(sendResult)){
+            System.out.println("[네트워크 오류 발생] 오류 코드 : " + connectResult);
+            return;
+        }
+
+        client.disconnect();
+    }
+
+    // 오류인지 확인
+    private static boolean isError(String connectResult) {
+        return !connectResult.equals("success");
+    }
+}
